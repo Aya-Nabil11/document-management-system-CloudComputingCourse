8baac7ee8410f16017d76ab549a0e5289e5a3ab0
Aya A. <alharazinaya@gmail.com>
2025-06-17 15:14:21 +0300

    aya

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,9 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 
 
-Route::get('/', function () {
-    return view('documents.index');
-});
 
-// Route::get('/', [DocumentController::class , 'index'])->name('dashboard');
+
+Route::get('/', [DocumentController::class , 'index'])->name('dashboard');
 
 Route::get('/documents/search', [DocumentController::class, 'searchView'])
     ->name('documents.search');
